2bd260fb8a5801615b45e6c98750c6f47831ef9b
Cyril Sadovnik <cyril.sadovnik@gmail.com>
2017-03-28 18:08:26 +0300

    Added rspec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,25 @@
+ENV['RAILS_ENV'] ||= 'test'
+require File.expand_path('../../config/environment', __FILE__)
+
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+
+require 'spec_helper'
+require 'rspec/rails'
+
+# Checks for pending migration and applies them before tests are run.
+ActiveRecord::Migration.maintain_test_schema!
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
+  config.use_transactional_fixtures = true
+
+  # Automatically mix in different behaviours tests
+  # https://relishapp.com/rspec/rspec-rails/docs
+  config.infer_spec_type_from_file_location!
+
+  # Filter lines from Rails gems in backtraces
+  # https://relishapp.com/rspec/rspec-rails/docs/backtrace-filtering
+  config.filter_rails_from_backtrace!
+end
