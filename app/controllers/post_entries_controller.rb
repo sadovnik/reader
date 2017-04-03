cf0bee46375213d58665aeaa4f922e965d5ed110
Cyril Sadovnik <cyril.sadovnik@gmail.com>
2017-04-03 14:49:17 +0300

    Fixed subscription order

--- app/controllers/post_entries_controller.rb
+++ app/controllers/post_entries_controller.rb
@@ -4,5 +4,7 @@ class PostEntriesController < ApplicationController
   # GET /feed
   def index
     @entries = current_user.entries
+      .includes(:post, :post => :source)
+      .order('Posts.published_at DESC')
   end
 end
