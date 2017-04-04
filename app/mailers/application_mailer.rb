19fc8774e3d559d23f845cef0d684a1c15b6c8ba
Cyril Sadovnik <cyril.sadovnik@gmail.com>
2017-04-04 20:27:12 +0300

    Fixed ApplicationMailer's "from" setting

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'from@example.com'
+  default from: "robot@#{ENV['HOST']}"
   layout 'mailer'
 end
--- app/mailers/invite_mailer.rb
+++ app/mailers/invite_mailer.rb
@@ -1,6 +1,4 @@
 class InviteMailer < ApplicationMailer
-  default from: 'invites@example.com'
-
   def invite(invite)
     @invite = invite
     mail(to: @invite.email, subject: 'Invite to Reader')
