e862258525ca209fcca249c0dd9d855cc2b65a17
Cyril Sadovnik <cyril.sadovnik@gmail.com>
2017-04-04 23:50:28 +0300

    Entry view improvements

--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -1,8 +1,27 @@
 $(function() {
-  $('.entry__link').click(function(e) {
+  $entryLinks = $('.entry__link');
+
+  function fetchReadLink($element) {
+    $readLink = $element.parent().find('.entry__read-link');
+
+    if ($readLink.length === 0) {
+      throw 'Unable to find read-link'
+    }
+
+    return $readLink[0];
+  }
+
+  $entryLinks.click(function(e) {
     e.preventDefault();
-    $readLink = $(e.target).parent().find('.entry__read-link')[0];
+
+    $originalLink = $(e.target);
+
+    $entry = $originalLink.parent().parent();
+    $entry.addClass('entries__entry_muted');
+
+    $readLink = fetchReadLink($originalLink);
     $readLink.click();
+
     return false;
   });
 })
