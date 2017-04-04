4225326c461ed479233ea7715008c8263c1812a4
Cyril Sadovnik <cyril.sadovnik@gmail.com>
2017-04-04 19:29:18 +0300

    feed.js: ES6 -> ES5 : (

--- app/assets/javascripts/feed.js
+++ app/assets/javascripts/feed.js
@@ -1,5 +1,5 @@
-$(() => {
-  $('.entry__link').click((e) => {
+$(function() {
+  $('.entry__link').click(function(e) {
     e.preventDefault();
     $readLink = $(e.target).parent().find('.entry__read-link')[0];
     $readLink.click();
