0f66abc6ec0715e10fead25e578d212036f19cd6
Cyril Sadovnik <cyril.sadovnik@gmail.com>
2017-04-05 15:35:54 +0300

    Fixed tests

--- spec/controllers/post_entries_controller_spec.rb
+++ spec/controllers/post_entries_controller_spec.rb
@@ -4,7 +4,7 @@ describe PostEntriesController do
   describe 'PUT /feed/entries/:id/status' do
     context 'not authorized user' do
       it 'redirects to the root' do
-        get :update_status, params: { id: 123 }
+        put :update_status, params: { id: 123 }
 
         expect(response).to redirect_to(root_path)
       end
@@ -22,26 +22,26 @@ describe PostEntriesController do
         # TODO: seems like i need to start using factory_girl or something
 
         it 'marks entry as read by default' do
-          get :update_status, params: { id: entry.id }
+          put :update_status, params: { id: entry.id }
           entry.reload
 
           expect(entry.read?).to be_truthy
         end
 
         it 'updates status if :status param provided' do
-          get :update_status, params: { id: entry.id, status: 'read' }
+          put :update_status, params: { id: entry.id, status: 'read' }
           entry.reload
 
           expect(entry.read?).to be_truthy
 
-          get :update_status, params: { id: entry.id, status: 'unread' }
+          put :update_status, params: { id: entry.id, status: 'unread' }
           entry.reload
 
           expect(entry.unread?).to be_truthy
         end
 
         it 'redirects to /feed' do
-          get :update_status, params: { id: entry.id }
+          put :update_status, params: { id: entry.id }
 
           expect(response).to redirect_to(feed_path)
         end
@@ -49,7 +49,7 @@ describe PostEntriesController do
 
       context 'not existing entry' do
         it 'returns 404' do
-          expect { get :update_status, params: { id: 123 } }.to raise_error(ActiveRecord::RecordNotFound)
+          expect { put :update_status, params: { id: 123 } }.to raise_error(ActiveRecord::RecordNotFound)
         end
       end
     end
