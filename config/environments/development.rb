8bc6a5ad55da5b992813643d184a577e74e57103
Cyril Sadovnik <cyril.sadovnik@gmail.com>
2017-04-06 15:43:05 +0300

    Set proper host for action_mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -31,7 +31,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => ENV['HOST'] || "localhost:#{ENV['PORT']}" }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
