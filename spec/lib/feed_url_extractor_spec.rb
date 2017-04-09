db02c6ad0bcb3a24b611ae5c04d1d762c8d051f2
Cyril Sadovnik <cyril.sadovnik@gmail.com>
2017-04-09 22:42:48 +0300

    Added FeedUrlExtractor

---
+++ spec/lib/feed_url_extractor_spec.rb
@@ -0,0 +1,32 @@
+require 'feed_url_extractor'
+
+# TODO: move to libhelper
+def extract_fixture(name)
+  path = File.expand_path("./fixtures/feed_url_extractor/#{name}.html", __dir__)
+  File.read(path)
+end
+
+describe FeedUrlExtractor do
+  describe '#extract_first' do
+    regular_cases = { 'jvns.ca' => '/atom.xml',
+                      'ilyabirman.ru' => 'http://ilyabirman.ru/meanwhile/rss/' }
+
+    regular_cases.each do |page, expected|
+      it "extracts first occured feed url (expample #{page})" do
+        page = extract_fixture(page)
+
+        actual = FeedUrlExtractor.extract_first(page)
+
+        assert_equal actual, expected
+      end
+    end
+
+    it 'returns `nil` if no feed found' do
+      page = extract_fixture('reader-app.herokuapp.com')
+
+      actual = FeedUrlExtractor.extract_first(page)
+
+      assert_nil actual
+    end
+  end
+end
