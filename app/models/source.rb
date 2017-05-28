ba2c275ca256ed880df7af3ed1d3fe02e6a6eda8
Cyril Sadovnik <cyril.sadovnik@gmail.com>
2017-05-28 14:38:43 +0300

    Source dependent destroy

--- app/models/source.rb
+++ app/models/source.rb
@@ -15,7 +15,7 @@
 class Source < ApplicationRecord
   alias_attribute :subscribers, :users
 
-  has_many :posts
-  has_many :subscriptions
+  has_many :posts, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
   has_many :users, through: :subscriptions
 end
